5aa19459922f2104c4a9515e12dbbe792d954425
Arpan Abhishek <arpanforbusiness@gmail.com>
2020-12-29 20:26:58 +0530

    Complete stage 2/5: The user is the gamemaster

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        System.out.print("X ");
-        System.out.print("O ");
-        System.out.println("X ");
-        System.out.print("O ");
-        System.out.print("X ");
-        System.out.println("O ");
-        System.out.print("X ");
-        System.out.print("X ");
-        System.out.println("O ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter cells:");
+        String inputString = scanner.nextLine();
+        String[] pattern = inputString.split("(?!^)");
+        System.out.print("---------");
+        for (int i = 0; i < pattern.length; i++){
+            if(i % 3 == 0) {
+                System.out.println();
+                System.out.print("| ");
+            }
+            System.out.print(pattern[i] + " ");
+            if((i+1)%3==0) System.out.print("|");
+        }
+        System.out.println();
+        System.out.println("---------");
     }
 }
